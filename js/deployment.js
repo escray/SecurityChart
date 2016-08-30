46d6450db991e43112a1c6f57d9a9febe05e2839
escray <zescray@gmail.com>
2016-08-30 12:02:54 +0800

    angular tabs

---
+++ js/deployment.js
@@ -0,0 +1,38 @@
+(function() {
+  var app = angular.module('deploy', []);
+
+  app.controller('DetailController', function(){
+     this.localDetail = items[1];
+  });
+
+  app.controller("PanelController", function(){
+    this.tab = 1;
+
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+
+    this.isSelected = function(checkTab) {
+      return this.tab === checkTab;
+    };
+  });
+
+
+})();
+
+var items = [
+  {
+    name: '浙江省',
+    net: '192.168.1.1',
+    timer: '24801 分钟',
+    count: 10573,
+    uninstall: 78
+  },
+  {
+    name: '陕西省',
+    net: '192.168.1.1',
+    timer: '10080 分钟',
+    count: 2093,
+    uninstall: 50
+  },
+  ];
