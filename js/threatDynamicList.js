a39714012ed2854da7579c1e1279255d723e7cb7
escray <zescray@gmail.com>
2016-12-05 06:45:02 +0800

    test sql.js

--- js/threatDynamicList.js
+++ js/threatDynamicList.js
@@ -1,3 +1,22 @@
 /**
  * Created by escray on 04/12/2016.
  */
+(function(){
+  var app = angular.module('threatDynamicList', []);
+
+  app.controller('DetailController', function(){
+
+  });
+
+  app.controller('PanelController', function(){
+    this.tab = 1;
+
+    this.isSet = function(checkTab) {
+      return this.tab === checkTab;
+    };
+
+    this.setTab = function(activeTab) {
+      this.tab = activeTab;
+    }
+  });
+})();
