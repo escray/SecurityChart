93aaa89830f797b0806fd198f283b55e18e3b559
escray <zescray@gmail.com>
2016-12-09 14:12:02 +0800

    six table complete

--- js/threatDynamicList.js
+++ js/threatDynamicList.js
@@ -1,21 +1,21 @@
 /**
  * Created by escray on 04/12/2016.
  */
-(function(){
+(function () {
   var app = angular.module('threatDynamicList', []);
 
-  app.controller('DetailController', function(){
+  app.controller('DetailController', function () {
 
   });
 
-  app.controller('PanelController', function(){
+  app.controller('PanelController', function () {
     this.tab = 1;
 
-    this.isSet = function(checkTab) {
+    this.isSet = function (checkTab) {
       return this.tab === checkTab;
     };
 
-    this.setTab = function(activeTab) {
+    this.setTab = function (activeTab) {
       this.tab = activeTab;
     }
   });
@@ -23,6 +23,7 @@
 
   console.log("ready");
   var ds;
+
   function loadBinaryFile(path, success) {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", path, true);
@@ -38,20 +39,109 @@
 
   loadBinaryFile('/data/security.db', function (data) {
     var sqldb = new SQL.Database(data);
-    var res = sqldb.exec("SELECT dwNo,guidComputer,guidCenter,dwIP,strProcessName FROM PlatformLogVirus");
-    ds = res[0].values;
+    var resAttack = sqldb.exec("SELECT dwNo,guidComputer,guidCenter,dwIP, dwProtocol,dwRemoteIP,dwDLocalPort,nDealResult,tmDefendTime,tmAddTime FROM PlatformLogAttack");
+    var resClient = sqldb.exec("SELECT guidComputer,guidCenter,guidGroup,strComputerName,dwIP,dwMAC,dwSubNetMask,strProductVersion,strVirusLibrayVersion,dwOperateSystem FROM PlatformClientInfo");
+    var resCenter = sqldb.exec("SELECT dwIP, strMAC,strComputerName,strCenterName,nState,nCenterLevel,tmRegisterTime,tmLastActiveTime,tmLogonOffTime FROM PlatformCenter");
+    // dwNo,guidComputer,guidCenter,dwIP,strProcessName,strProcessCreater,nUnknownFlag,nKnowVirusType,strVirusName,nUnknowVirusType,dwVirusCode,nDetector,nDiskType,nDealResult,tmFindTime,tmAddTime,tmAddPlatformTime
+    var resVirus = sqldb.exec("SELECT dwNo,guidComputer,guidCenter,dwIP,strProcessName,strProcessCreater,nUnknownFlag,nDealResult,tmFindTime,tmAddTime FROM PlatformLogVirus");
 
-    $('#example1').DataTable( {
-      data: ds,
+    // dwNo,guidComputerID,guidCenterID,dwIP,strMAC,strProcessName,nDealResult,tmFindTime,tmAddTime,tmAddPlatformTime
+    var resOverflow = sqldb.exec("SELECT dwNo,guidComputerID,guidCenterID,dwIP,strMAC,strProcessName,nDealResult,tmFindTime,tmAddTime FROM PlatformLogOverFlow");
+    //guidGroup,guidCenter,strGroupName,strGroupPath,strDescription,nUpdateLimit,tmStateChangeTime
+    var resGroup = sqldb.exec("SELECT guidGroup,guidCenter,strGroupName,strGroupPath,strDescription,nUpdateLimit FROM PlatformGroup");
+
+    $('#tblGroup').DataTable({
+      data: resGroup[0].values,
       columns: [
-        { title: "Name" },
-        { title: "Position" },
-        { title: "Office" },
-        { title: "Extn." },
-//        { title: "Start date" },
-        { title: "Salary" }
+        {title: "组编号"},
+        {title: "所属中心名称"},
+        {title: "组名称"},
+        {title: "所属组"},
+        {title: "组信息描述"},
+        {title: "组升级限制策略"}
       ]
-    } );
-  } );
+    });
+
+    $('#tblOverFlow').DataTable({
+      data: resOverflow[0].values,
+      columns: [
+        {title: "编号"},
+        {title: "计算机名称"},
+        {title: "所属中心"},
+        {title: "IP地址"},
+        {title: "MAC地址"},
+        {title: "被溢出程序名称"},
+        {title: "处理结果"},
+        {title: "拦截时间"},
+        {title: "上报时间"}
+      ]
+    });
+
+    $('#tblVirus').DataTable({
+      data: resVirus[0].values,
+      columns: [
+        {title: "编号"},
+        {title: "计算机名称"},
+        {title: "所属中心"},
+        {title: "IP地址"},
+        {title: "恶意程序文件名"},
+        {title: "创建恶意程序的文件名"},
+        {title: "已知/新型类别"},
+        {title: "处理结果"},
+        {title: "拦截时间"},
+        {title: "上报时间"}
+      ]
+    });
+
+    $('#tblAttack').DataTable({
+      data: resAttack[0].values,
+      columns: [
+        {title: "编号"},
+        {title: "计算机名称"},
+        {title: "所属中心"},
+        {title: "被攻击主机IP地址"},
+        {title: "攻击所用协议类型"},
+        {title: "攻击方IP地址"},
+        {title: "被攻击端口号"},
+        {title: "处理结果"},
+        {title: "拦截时间"},
+        {title: "上报时间"}
+      ]
+    });
+
+    $('#tblCenter').DataTable({
+      data: resCenter[0].values,
+      columns: [
+        {title: "IP地址"},
+        {title: "MAC地址"},
+        {title: "中心名称"},
+        {title: "中心别名"},
+        {title: "在线状态"},
+        {title: "中心级别"},
+        {title: "中心部署时间"},
+        {title: "最后激活时间"},
+        {title: "离线时间"}
+      ]
+    });
+
+    $('#tblClient').DataTable({
+      data: resClient[0].values,
+      columns: [
+        {title: "计算机标识"},
+        {title: "所属中心"},
+        {title: "所属分组"},
+        {title: "计算机名称"},
+        {title: "IP地址"},
+        {title: "MAC地址"},
+        {title: "网络掩码"},
+        {title: "程序版本号"},
+        {title: "特征码库版本号"},
+        {title: "操作系统类型"}
+        // {title: "防御状态"},
+        // {title: "防火墙开启状态"},
+        // {title: "防火墙规则"},
+      ]
+    });
+  });
 })();
 
